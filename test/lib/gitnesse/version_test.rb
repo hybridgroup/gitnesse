ec2893b674b94598bd79419e9e9701ae87b648c6
Luis Hurtado <luis@hlmbusiness.com>
2012-11-02 11:10:11 -0500

    Test helper and basic test.

---
+++ test/lib/gitnesse/version_test.rb
@@ -0,0 +1,7 @@
+require_relative '../../test_helper'
+
+describe Gitnesse do
+  subject { Gitnesse::VERSION }
+
+  it { wont_be_nil }
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+require "minitest/matchers"
+require "minitest/autorun"
+require "minitest/pride"
+require File.expand_path('../../lib/gitnesse.rb', __FILE__)
