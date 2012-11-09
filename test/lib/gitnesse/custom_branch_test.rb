4713470ed865471d0c988e00c92a3a46dd6b7a05
Andrew Stewart <andrew@averagestudios.com>
2012-11-09 10:49:46 -0800

    Fixed bug where tests would sometimes not pass

--- test/lib/gitnesse/custom_branch_test.rb
+++ test/lib/gitnesse/custom_branch_test.rb
@@ -2,21 +2,24 @@ require_relative '../../test_helper'
 
 describe Gitnesse do
   describe "#branch" do
-    it "defaults to 'master'" do
-      Gitnesse.branch.must_equal "master"
+    describe "when default to 'master'" do
+      before { Gitnesse.branch = nil }
+      it { Gitnesse.branch.must_equal "master" }
     end
 
-    it "can be changed" do
-      Gitnesse.branch = "wiki"
-      Gitnesse.branch.must_equal "wiki"
+    describe "when changed" do
+      before { Gitnesse.branch = "wiki" }
+      it { Gitnesse.branch.must_equal "wiki" }
     end
 
-    it "can be changed through #config" do
-      Gitnesse.config do |config|
-        config.branch = "wiki"
+    describe "when changed through #config" do
+      before do
+        Gitnesse.config do |config|
+          config.branch = "wiki"
+        end
       end
 
-      Gitnesse.branch.must_equal "wiki"
+      it { Gitnesse.branch.must_equal "wiki" }
     end
   end
 end
