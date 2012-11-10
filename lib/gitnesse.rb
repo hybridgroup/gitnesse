da7a32397942f1c4cd574cfb1ca83c784a1ef5b0
Luis Hurtado <luis@hlmbusiness.com>
2012-11-09 22:43:06 -0500

    Enable loading config searching recursively for gitnesse.rb files under the current directory.

--- lib/gitnesse.rb
+++ lib/gitnesse.rb
@@ -247,4 +247,29 @@ module Gitnesse
   def ensure_repository
     raise "You must select a repository_url to run Gitnesse." if Gitnesse.repository_url.nil?
   end
+
+  def load_config
+    load(ENV['GITNESSE_CONFIG']) and return if ENV['GITNESSE_CONFIG']
+
+    possible_config_files = Dir.glob(File.join("**", "gitnesse.rb"))
+
+    files_with_config = possible_config_files.select do |file_name|
+      if FileUtils.compare_file(__FILE__, file_name)
+        false
+      else
+        file_content = File.read(file_name)
+        file_content.match("Gitnesse.config")
+      end
+    end
+
+    case files_with_config.length
+      when 0
+        raise "Can't find a gitnesse.rb file with Gitnesse configuration."
+      when 1
+        load(File.absolute_path(files_with_config.first))
+      else
+        raise "Several config files found: #{files_with_config.join(", ")}"
+    end
+  end
+
 end
