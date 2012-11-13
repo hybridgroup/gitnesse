8d25234aa1b0b782415aa44865a1744315380d3b
Andrew Stewart <andrew@averagestudios.com>
2012-11-13 13:40:51 -0800

    Load config before running rake tasks

--- lib/gitnesse/tasks.rb
+++ lib/gitnesse/tasks.rb
@@ -7,16 +7,19 @@ module Gitnesse
       namespace :gitnesse do
         desc "Pull features from remote repository and run cucumber."
         task :run => :environment do
+          Gitnesse.load_config
           Gitnesse.run
         end
 
         desc "Pull features from remote git wiki repository."
         task :pull => :environment do
+          Gitnesse.load_config
           Gitnesse.pull
         end
 
         desc "Push features to remote git wiki repository."
         task :push => :environment do
+          Gitnesse.load_config
           Gitnesse.push
         end
       end
