b22775a96cbf600a8fc370f29b627284ac232fbd
Andrew Stewart <andrew@averagestudios.com>
2012-11-13 11:38:28 -0800

    Added test helper modules

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,24 @@ require "minitest/autorun"
 require "minitest/pride"
 require "mocha"
 require File.expand_path('../../lib/gitnesse.rb', __FILE__)
+
+module FeatureTestMethods
+
+  # Creates a basic test feature
+  #
+  # Returns a string
+  def create_test_feature
+    <<-EOF
+Feature: Addition
+  In order to avoid silly mistakes
+  As a math idiot
+  I want to be told the sum of two numbers
+
+  Scenario: Add two numbers
+    Given I have entered 50 into the calculator
+    And I have entered 70 into the calculator
+    When I press add
+    Then the result should be 120 on the screen
+    EOF
+  end
+end
--- test/wiki_test_helper.rb
+++ test/wiki_test_helper.rb
@@ -1,1 +1,11 @@
 require "gollum"
+
+module WikiTestMethods
+
+  # Creates a new Gollum wiki for testing
+  #
+  # Returns a gollum wiki
+  def create_wiki(dir = Dir.mktmpdir)
+    Gollum::Wiki.new(dir)
+  end
+end
