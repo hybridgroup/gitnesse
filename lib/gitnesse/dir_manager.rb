57821cea47170d57e30111e5902e85ee3c3a3388
Andrew Stewart <andrew@stwrt.ca>
2013-08-26 12:07:59 -0700

    Refactor of DirManager, added .project_dir_present?

--- lib/gitnesse/dir_manager.rb
+++ lib/gitnesse/dir_manager.rb
@@ -7,18 +7,31 @@ module Gitnesse
     #
     # Returns the path to the created project dir
     def self.make_project_dir
-      project_name = File.basename Dir.pwd
-      path = "#{Dir.home}/.gitnesse/#{project_name}"
-      FileUtils.mkdir_p path
-      path
+      FileUtils.mkdir_p project_dir
+      project_dir
     end
 
     # Public: Removes project dir, but not ~/.gitnesse dir
     #
     # Returns nothing
     def self.remove_project_dir
+      FileUtils.rm_rf project_dir
+    end
+
+    # Public: Checks that project directory is present
+    #
+    # Returns a boolean indicating if the dir exists or not
+    def self.project_dir_present?
+      File.directory? project_dir
+    end
+
+    private
+    # Private: Constructs project dir path in ~/.gitnesse folder
+    #
+    # Returns a string path
+    def self.project_dir
       project_name = File.basename Dir.pwd
-      FileUtils.rm_rf "#{Dir.home}/.gitnesse/#{project_name}"
+      "#{Dir.home}/.gitnesse/#{project_name}"
     end
   end
 end
--- spec/lib/dir_manager_spec.rb
+++ spec/lib/dir_manager_spec.rb
@@ -2,22 +2,44 @@ require 'spec_helper'
 
 module Gitnesse
   describe DirManager do
-    let(:path) { Dir.home }
+    let(:path) { "#{Dir.home}/.gitnesse/gitnesse" }
 
     describe ".make_project_dir" do
       before do
-        FileUtils.should_receive(:mkdir_p).with("#{path}/.gitnesse/gitnesse").and_return(true)
+        FileUtils.should_receive(:mkdir_p).with(path).and_return(true)
       end
 
       it "creates a dir based on the current project name" do
-        expect(DirManager.make_project_dir).to eq "#{path}/.gitnesse/gitnesse"
+        expect(DirManager.make_project_dir).to eq path
       end
     end
 
     describe ".remove_project_dir" do
       it "removes the wiki dir for the current project" do
-        FileUtils.should_receive(:rm_rf).with("#{path}/.gitnesse/gitnesse").and_return(true)
+        FileUtils.should_receive(:rm_rf).with(path).and_return(true)
         DirManager.remove_project_dir
+      end
+    end
+
+    describe ".project_dir_present?" do
+      context "if project dir is a directory" do
+        before do
+          File.should_receive(:directory?).with(path).and_return(true)
+        end
+
+        it "returns true" do
+          expect(DirManager.project_dir_present?).to be_true
+        end
+      end
+
+      context "if project dir does not exists" do
+        before do
+          File.should_receive(:directory?).with(path).and_return(false)
+        end
+
+        it "returns false" do
+          expect(DirManager.project_dir_present?).to be_false
+        end
       end
     end
   end
