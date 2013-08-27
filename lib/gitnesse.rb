0b5549dbacb22d6b8e793dae8f6cc7f690711c5d
Andrew Stewart <andrew@stwrt.ca>
2013-08-27 10:46:11 -0700

    Remove requires for no-longer-used classes

--- lib/gitnesse.rb
+++ lib/gitnesse.rb
@@ -1,11 +1,9 @@
 require 'gitnesse/version'
-require 'gitnesse/commit_info_generator'
 require 'gitnesse/config'
 require 'gitnesse/config_loader'
 require 'gitnesse/dependency_checker'
 require 'gitnesse/feature_extractor'
 require 'gitnesse/feature_transform'
-require 'gitnesse/git_config_reader'
 require 'gitnesse/dir_manager'
 require 'gitnesse/wiki'
 
